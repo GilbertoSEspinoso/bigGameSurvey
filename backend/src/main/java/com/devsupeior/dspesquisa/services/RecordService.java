ebd7cef13cdfc00c759c23ad2c0c8aa9481192ad
Gilberto Espinoso <gilbertosilvaespinosa@gmail.com>
2023-07-08 12:17:20 -0300

    GET records

--- backend/src/main/java/com/devsupeior/dspesquisa/services/RecordService.java
+++ backend/src/main/java/com/devsupeior/dspesquisa/services/RecordService.java
@@ -1,5 +1,6 @@
 package com.devsupeior.dspesquisa.services;
 
+import com.devsupeior.dspesquisa.dto.GameDTO;
 import com.devsupeior.dspesquisa.dto.RecordDTO;
 import com.devsupeior.dspesquisa.dto.RecordInsertDTO;
 import com.devsupeior.dspesquisa.entities.Game;
@@ -7,10 +8,14 @@ import com.devsupeior.dspesquisa.entities.Record;
 import com.devsupeior.dspesquisa.repositories.GameRepository;
 import com.devsupeior.dspesquisa.repositories.RecordRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.time.Instant;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class RecordService {
@@ -34,4 +39,8 @@ public class RecordService {
         return new RecordDTO(entity);
     }
 
+@Transactional(readOnly = true)
+    public Page<RecordDTO> findByMoments(Instant minDate, Instant maxDate, PageRequest pageRequest) {
+        return repository.findByMoments(minDate,maxDate,pageRequest).map(x -> new RecordDTO(x));
+    }
 }
